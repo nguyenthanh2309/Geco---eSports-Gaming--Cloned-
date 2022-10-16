9347d748adf550bbc10be481eab70dd2851ca6fa
nguyenthanh2309 <107359526+nguyenthanh2309@users.noreply.github.com>
2022-10-16 22:32:15 +0700

    ✨feature: updated

--- js/app.js
+++ js/app.js
@@ -167,7 +167,6 @@ function renderProductsCategoryData() {
         });
 }
 
-
 renderReleashedGameData();
 renderFeatureGamesData();
 renderGamingProductsData();
--- js/event_handle.js
+++ js/event_handle.js
@@ -1,8 +1,10 @@
 function navItemHandleEvent() {
     const navItems = document.querySelectorAll(".nav__item");
 
-    navItems.forEach(item => item.addEventListener('mouseover', () => item.classList.add('nav--active')));
-    navItems.forEach(item => item.addEventListener('mouseleave', () => item.classList.remove('nav--active')));
+    for (let i = 1; i < navItems.length; i++) {
+        navItems[i].addEventListener('mouseover', () => navItems[i].classList.add('nav--active'));
+        navItems[i].addEventListener('mouseleave', () => navItems[i].classList.remove('nav--active'));
+    }
 }
 
 function dropdownHandleEvent() {
@@ -19,15 +21,26 @@ function dropdownHandleEvent() {
 
     function displayDropdown(navItem, dropdown) {
         navItem.classList.add('nav--active');
-        dropdown.style.display = 'block';
+        dropdown.style.animation = 'dropdownAppearence .2s linear';
+        setTimeout(() => dropdown.style.display = 'block', 100);
     }
 
     function hideDropdown(navItem, dropdown) {
         navItem.classList.remove('nav--active');
-        dropdown.style.display = 'none';
+        dropdown.style.animation = 'dropdownDisappearence .2s linear';
+        setTimeout(() => dropdown.style.display = 'none', 100);
     }
 
-    for (let i = 0; i < itemsHasDropdown.length; i++) {
+    itemsHasDropdown[0].addEventListener('mouseover', () => {
+        dropdownMenus[0].style.animation = 'dropdownAppearence .2s linear';
+        setTimeout(() => dropdownMenus[0].style.display = 'block', 100);
+    });
+    itemsHasDropdown[0].addEventListener('mouseleave', () => {
+        dropdownMenus[0].style.animation = 'dropdownDisappearence .2s linear';
+        setTimeout(() => dropdownMenus[0].style.display = 'none', 100);
+    });
+
+    for (let i = 1; i < itemsHasDropdown.length; i++) {
 
         multipleElementsEvent([itemsHasDropdown[i], dropdownMenus[i]], 'mouseover', displayDropdown.bind(null, itemsHasDropdown[i], dropdownMenus[i]));
 
@@ -35,5 +48,27 @@ function dropdownHandleEvent() {
     }
 }
 
+function sliderHandleEvent() {
+    const sliderBtns = document.querySelectorAll(".slider__btn");
+    const sliderItem = document.querySelector(".slider__item");
+    const sliderTexts = sliderItem.querySelectorAll('h3, h1, p');
+
+    function slideTextAppear() {
+        for (let i = 0; i < sliderTexts.length - 1; i++) {
+            sliderTexts[i].style.animation = 'slideTextChanging .8s ease-in-out';
+        }
+        sliderTexts[sliderTexts.length - 1].style.animation = 'slideTextChanging .8s ease-in-out .2s';
+    }
+    sliderBtns.forEach(btn => btn.addEventListener('click', () => {
+        if (!btn.style.animation) {
+            slideTextAppear();
+        } else {
+            sliderTexts.forEach(text => text.style.removeProperty('animation'));
+            console.log('remove')
+        }
+    }));
+}
+
 navItemHandleEvent();
 dropdownHandleEvent();
+sliderHandleEvent();
